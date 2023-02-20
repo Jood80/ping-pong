65e9eba7f393e22f0fe066351e1686209c39f08a
Jood <56412800+Jood80@users.noreply.github.com>
2023-02-20 22:24:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-import {createServer} from 'http'
-import {Server} from 'socket.io'
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const httpServer = createServer()
+const httpServer = createServer();
 
 const io = new Server(httpServer, {
   cors: {
-		origin: "http://localhost:5173",
-		credentials: true,
-		  methods: ['GET', 'POST']
-  }
-})
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
 
-let playersCount = 0
-io.on('connection', (socket) => {
-	console.log(`User ${socket.id} is connected`);
+let playersCount = 0;
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} is connected`);
 
-	
-	socket.on('ready', () => {
-		console.log(`player ${ socket.id } is ready`);
-		
-		playersCount++
+  socket.on("ready", () => {
+    console.log(`player ${socket.id} is ready`);
 
-		if(playersCount % 2 === 0) {
-			io.emit('startGame', socket.id)
-		}
-	})
+    playersCount++;
 
-	socket.on('paddleMove', (paddleData) => {
-		socket.broadcast.emit('paddleMove', paddleData)
-	})
+    if (playersCount % 2 === 0) {
+      io.emit("startGame", socket.id);
+    }
+  });
 
-	socket.on('ballMove', (ballData) => {
-		socket.broadcast.emit('ballMove', ballData)
-	})
+  socket.on("paddleMove", (paddleData) => {
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
 
-	socket.on('disconnect', (reason) => {
-		console.log(`client ${socket.id} disconnected due to: ${reason}` );
-	})
-})
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
 
-httpServer.listen(3000, () => console.log("listening on port 3000"))
+  socket.on("disconnect", (reason) => {
+    console.log(`client ${socket.id} disconnected due to: ${reason}`);
+  });
+});
+
+httpServer.listen(3000, () => console.log("listening on port 3000"));
